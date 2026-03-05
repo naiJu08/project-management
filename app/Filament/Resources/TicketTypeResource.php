bc7f4fe8fc815bfb55cb06daf89c78716f0b9b87
abhinou.inovace <abhinou.inovace@gmail.com>
2026-03-05 15:31:41 +0530

    ticket type fixed multiple create and return back to list

--- app/Filament/Resources/TicketTypeResource.php
+++ app/Filament/Resources/TicketTypeResource.php
@@ -12,6 +12,7 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Guava\FilamentIconPicker\Forms\IconPicker;
 use Guava\FilamentIconPicker\Tables\IconColumn;
+use Illuminate\Validation\Rule;
 
 class TicketTypeResource extends Resource
 {
@@ -47,7 +48,11 @@ class TicketTypeResource extends Resource
                                 Forms\Components\TextInput::make('name')
                                     ->label(__('Type name'))
                                     ->required()
-                                    ->maxLength(255),
+                                    ->maxLength(255)
+                                    ->rule(
+                                Rule::unique('ticket_types', 'name')
+                                    ->whereNull('deleted_at')
+                            ),
 
                                 Forms\Components\ColorPicker::make('color')
                                     ->label(__('Type color'))
