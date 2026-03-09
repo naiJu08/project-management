e076b646d868a969ae38c370429bb45f25067205
abhinou.inovace <abhinou.inovace@gmail.com>
2026-03-09 11:14:12 +0530

    roles create return back to list and role duplicate fixed

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -46,7 +46,7 @@ class RoleResource extends Resource
                             ->schema([
                                 Forms\Components\TextInput::make('name')
                                     ->label(__('Permission name'))
-                                    ->unique(table: Permission::class, column: 'name')
+                                    ->unique(table: Role::class, column: 'name', ignoreRecord: true)
                                     ->maxLength(255)
                                     ->required(),
 
