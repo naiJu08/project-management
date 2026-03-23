225fd63f96d97525ee89e9cd0649036870e9446d
abhinou.inovace <abhinou.inovace@gmail.com>
2026-03-23 10:09:05 +0530

    fix : voice call sdp issues

--- app/Events/CallOffer.php
+++ app/Events/CallOffer.php
@@ -6,11 +6,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Queue\SerializesModels;
+// Remove: use Illuminate\Queue\SerializesModels;
 
 class CallOffer implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets; // Removed SerializesModels
 
     public $offer;
     public $callerId;
