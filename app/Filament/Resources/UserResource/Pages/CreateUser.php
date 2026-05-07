09940264ff14291333c94cb0120046d86a1180e9
abhinou.inovace <abhinou.inovace@gmail.com>
2026-05-07 12:10:00 +0530

    fix : user side 500 server error issues to create

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,32 +3,51 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Mail\NewUserMail;
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\NewUserMail;
+use Illuminate\Support\Str;
+use Throwable;
 
 class CreateUser extends CreateRecord
 {
     protected function afterCreate(): void
-{
-    Mail::to($this->record->email)
-        ->send(new NewUserMail($this->record, $this->plainPassword));
-}
+    {
+        try {
+            Mail::to(new Address($this->record->email, $this->record->name ?? ''))
+                ->send(new NewUserMail($this->record, $this->plainPassword));
+        } catch (Throwable $exception) {
+            Log::warning('New user created, but welcome email could not be sent.', [
+                'user_id' => $this->record->id,
+                'email' => $this->record->email,
+                'error' => $exception->getMessage(),
+            ]);
+
+            Notification::make()
+                ->warning()
+                ->title('User created, but email was not sent')
+                ->body('Please verify the SMTP settings and resend the welcome email if needed.')
+                ->send();
+        }
+    }
+
     protected static string $resource = UserResource::class;
 
     protected function mutateFormDataBeforeCreate(array $data): array
-{
-    $password = Str::random(10);
+    {
+        $password = Str::random(10);
 
-    $data['password'] = Hash::make($password);
+        $data['password'] = Hash::make($password);
 
-    $this->plainPassword = $password;
+        $this->plainPassword = $password;
 
-    return $data;
-}
+        return $data;
+    }
 
     protected function getRedirectUrl(): string
     {
